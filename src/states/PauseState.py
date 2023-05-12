a2948a2997f1eb47e59d5f526018f2025e110e36
Nomeelaa <gm5072@gmail.com>
2023-05-12 14:58:51 -0400

    Game Base using Gale

---
+++ src/states/PauseState.py
@@ -0,0 +1,20 @@
+""" 
+ISPPJ1 2023
+Study Case: Breakout
+
+Author: Alejandro Mujica
+alejandro.j.mujic4@gmail.com
+
+This file contains the class to define the Start state.
+"""
+import pygame
+
+from gale.input_handler import InputData
+from gale.state import BaseState
+from gale.text import render_text
+
+import settings
+
+
+class PauseState(BaseState):
+    pass
